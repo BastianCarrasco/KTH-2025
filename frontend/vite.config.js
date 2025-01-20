4e0c2fc2d791fd0116f096efea8b279ed3163a2b
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-01-20 16:33:54 -0300

    fisrt commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Incluir archivos JPG como assets
+  base: "/KTH-2025"
 });
